d9ede11d113a78906cb16271a967d1df37ca23db
Linus <s_widing21@stud.hwr-berlin.de>
2023-05-07 19:42:20 +0200

    adding SQLAlchemy support and User class

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class AppUser(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    email = db.Column("email", db.String(100))
+    pwd = db.Column("pwd", db.String(100))
+
+    def __init__(self, email, pwd):
+        self.email = email
+        self.pwd = pwd
 
 
 @app.route('/')
 def index():
+    db.create_all()
     return render_template("index.html")
 
