059585912b4f78d6db3a0354a01965f4172380dc
Linus Widing <s_widing21@stud.hwr-berlin.de>
2023-07-01 17:11:53 +0200

    Updating api endpoint for recipies
    Now accepts args to filter kcal and protein

--- api.py
+++ api.py
@@ -6,11 +6,26 @@ api = Blueprint("api", __name__)
 
 @api.route("/recipes/")
 def fetch_recipes():
-    counter = int(request.args.get("counter"))
-    limit = int(request.args.get("limit"))
-    res = []
-    recipes = Recipe.query.order_by(Recipe.id).paginate(page=counter, per_page=limit)
-    for r in recipes:
-        res.append(r.to_dict(rules=("img_path", "total_kcal", "ingredients_amount", "total_protein")))
-    return res
 
+    recipes = Recipe.query.order_by(Recipe.id)
+    if "min_kcal" in request.args:
+        min_kcal = int(request.args.get("min_kcal"))
+        recipes = recipes.filter(Recipe.total_kcal > min_kcal)
+
+    if "max_kcal" in request.args:
+        max_kcal = int(request.args.get("max_kcal"))
+        recipes = recipes.filter(Recipe.total_kcal < max_kcal)
+
+    if "min_protein" in request.args:
+        min_protein = int(request.args.get("min_protein"))
+        recipes = recipes.filter(Recipe.total_protein > min_protein)
+
+    if ("limit" and "counter") in request.args:
+        limit = int(request.args.get("limit"))
+        counter = int(request.args.get("counter"))
+        recipes = recipes.paginate(page=counter, per_page=limit)
+
+    res = []
+    for r in recipes:
+        res.append(r.to_dict(rules=("img_path", "ingredients_amount")))
+    return res
